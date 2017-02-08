69c9a8aa5d2ce050d1987d7a92504327836e4adb
Admin <vanpho02@gmail.com>
2017-02-08 20:37:49 +0700

    ejs

--- index.js
+++ index.js
@@ -5,4 +5,14 @@ app.set('views', './views');
 app.use(express.static('public'));
 app.listen(3000, () => console.log('Server started'));
 
+var {getInfoById} = require('./db.js');
+
 app.get('/', (req, res) => res.render('home'));
+
+app.get('/show/:id', (req, res) => {
+  getInfoById(req.params.id, (err, result) => {
+    if(err) return res.send('Loi ' + err);
+    if(result.rows.length === 0) return res.send('Khong ton tai');
+    res.render('home', {girl: result.rows[0]});
+  });
+});
