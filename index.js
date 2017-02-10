2773d07b17b2a360241dfec0b6c3ea1c8d137036
Admin <vanpho02@gmail.com>
2017-02-10 20:43:40 +0700

    prepare for heroku

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
-app.listen(3000, () => console.log('Server started'));
+app.listen(process.env.PORT || 3000, () => console.log('Server started'));
 
 var {getInfoById, likeById, dislikeById} = require('./db.js');
 
